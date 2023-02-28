a6e870448d44853456f1297194126d555c8dafc1
Davi Ribeiro <phiuss@gmail.com>
2023-02-28 14:56:35 -0800

    Handling CORS

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import express, { Express } from "express";
-
+import cors from "cors";
 import getSummonerMatches from "./endpoints/getSummonerMatches";
 
 const app: Express = express();
 const port = process.env.LOL_API_PORT || 3001;
+
+app.use(
+  cors({
+    origin: process.env.LOL_CLIENT_APP_URL || "*",
+  })
+);
 
 app.get("/summoner/matches", getSummonerMatches);
 
